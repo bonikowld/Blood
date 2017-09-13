abff4129920e8fde6d9c9b2547711854add68c43
aljanlytayone <31721848+aljanlytayone@users.noreply.github.com>
2017-09-13 08:42:04 -0700

    gibutangan ug header taga page para inig click kay mu highlight kunuhay

--- search.php
+++ search.php
@@ -1,4 +1,31 @@
-<?php include('lib/header.php');?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link type="style/css" href="css/header.css" rel="stylesheet">
+<link rel="stylesheet" href="css/header.css" style="text/css">
+
+  
+<div class="container">
+
+    <!-- Must have a design here-->
+
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.php">Blood Seeker</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a></li>
+          <li class="active"><a href="search.php">Search Blood</a></li>
+          <li><a href="request.php">Request Blood</a></li>
+          <li><a href="donate.php">Donate Blood</a></li>
+        </ul>
+      
+
+ 
+    </div>
+    </nav>
+    </div>
 
 <title> BloodSeeker - Search Blood </title>
 <div class="container">
