57ff9c3617bc2a3bddee730440e12e8610525fe7
aljanlytayone <31721848+aljanlytayone@users.noreply.github.com>
2017-09-15 13:57:58 -0700

    initial changes

--- lib/donateblood.php
+++ lib/donateblood.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/accordionbutton.css" style="text/css">
 
 <br><br>
 <div id="exTab2" class="container">	
@@ -24,7 +25,8 @@
 				<div class="tab-pane" id="2">
           <h3 align="center">How to Donate Blood</h3>
           
-          <h4>Preparation before donating blood</h4>
+          <button class="accordion">Preparation before donating blood</button>
+          <div class="panel">
           <ul>
             <li>Have enough rest and sleep.</li>
             <li>No alcohol intake 24 hours prior to blood donation.</li>
@@ -32,8 +34,10 @@
             <li>Have something to eat prior to blood donation, avoid fatty food.</li>
             <li>Drink plenty of fluid, like water or juice.</li>
           </ul>
-          <hr>
-          <h4>Steps in donating blood</h4>
+          </div>
+
+          <button class="accordion">Steps in donating blood</button>
+          <div class="panel">
           <ul>
             <li>Have your weight taken.</li>
             <li>Register and honestly and complete the donor registration form.</li>
@@ -43,8 +47,10 @@
                 donors weight and blood pressure. It usually takes 10 minutes or less.</li>
             <li>A 5 to 10 minute rest and plenty of fluid-intake are necessary after donation.</li>
           </ul>
-          <hr>
-          <h4>What to do after blood donation?</h4>
+          </div>
+
+          <button class="accordion">What to do after blood donation?</button>
+          <div class="panel">
           <ul>
             <li>Drink plenty of fluid, like water or juice.</li>
             <li>Refrain from stooping down after blood donation.</li>
@@ -55,8 +61,10 @@
             <li>If there is discoloration and swelling on the punctured site, you may apply cold compress for 24 hours.</li>
             <li>If there is dizziness, just lie down with your feet elevated. Drink plenty of juice; and in just a few minutes or so, it will pass.</li>
           </ul>
-          <hr>
-          <h4>Blood Donation: Basic Requirements</h4>
+          </div>
+
+          <button class="accordion">Blood Donation: Basic Requirements</button>
+          <div class="panel">
               <p>Blood donation helps save lives. Find out if you are 
                 eligible to donate blood and what to expect if you decide to donate.
               </p>
@@ -68,8 +76,10 @@
               national blood requirements.If you are in good health, you can probably donate. Find out
               about blood donation and what to expect if you decide to give.
               </p>
-          <hr>
-          <h4>Who can donate blood?</h4>
+          </div>
+
+          <button class="accordion">Who can donate blood?</button>
+          <div class="panel">
           <ul>
             <li>Are in good health</li>
             <li>Are between 16 to 65 years old (16 and 17 years old need parents consent)</li>
@@ -77,54 +87,64 @@
             <li>Have a blood pressure between: Systolic: 90-160 mmHg, Diastolic: 60-100 mmHg</li>
             <li>Pass the physical and health history assessments.</li>
           </ul>
+          </div>
+				
+        </div>
 
-          <h4></h4>
-          <h5></h5>
-          <h5></h5>
-          <h5></h5>
-          <h5></h5>
-				</div>
         <div class="tab-pane" id="3">
           <h3 align="center">FAQs</h3>
 
-          <h4>What happens to my donated blood?</h4>
+          <button class="accordion">What happens to my donated blood?</button>
+          <div class="panel">
           <p align="justify">
           Each unit of blood collected will be examined for 5 transfusion-transmissible infectious diseases,
           namely: HIV, Malaria, Syphillis, Hepatitis B, and Hepatitis C before it is transfused to patients.
           </p>
-          <hr>
-          <h4>Is it safe to give blood?</h4>
+          </div>
+
+          <button class="accordion">Is it safe to give blood?</button>
+          <div class="panel">
           <p align="justify">
           Yes. The Red Cross ensures that donating blood is a safe opportunity to give the gift of life. Each
           needle used in the procedure is sterile and is disposed after a single use. It is important that all
           blood donors are in good health, well-rested, and have eaten prior to donation.
           </p>
-          <hr>
-          <h4>When can we donate blood?</h4>
+          </div>
+
+          <button class="accordion">When can we donate blood?</button>
+          <div class="panel">
           <p>
           A healthy person may donate blood every three months.
           </p>
-          <hr>
-          <h4>Where can I donate blood?</h4>
+          </div>
+
+          <button class="accordion">Where can I donate blood?</button>
+          <div class="panel">
           <p align="justify">
           You may come and visit PRCs National Blood Center, Regional Blood Centers, or any of its Blood Services
           Facilities, nationwide.
           </p>
-          <hr>
-          <h4>Can a person who has a tattoo donate blood?</h4>
+          </div>
+
+          <button class="accordion">Can a person who has a tattoo donate blood?</button>
+          <div class="panel">
           <p align="justify">
           As long as the tattooing procedure was done aseptically (in a sterile manner), he/ she may donate blood 
           one year after the procedure. This is the same with ear piercing, acupuncture, and other procedures involving
           needles.
           </p>
-          <hr>
-          <h4>Are the health history questions necessary every time I donate?</h4>
+          </div>
+
+          <button class="accordion">Are the health history questions necessary every time I donate?</button>
+          <div class="panel">
           <p align="justify">
           To ensure the safest possible blood supply, all donors must undergo the necessary screening every donation. The
           World Health Organization and the Department of Health require all blood centers to conform to this practice.
           </p>
-          <hr>
-          <h4>What does the term "donor deferral" mean?</h4>
+          </div>
+
+          <button class="accordion">What does the term "donor deferral" mean?</button>
+          <div class="panel">
           <p align="justify">
           Individuals disqualified from donating blood are known as "deferred" donors. A prospective donor may be 
           deferred at any point during the collection and testing process. Whether or not a person is deferred,
@@ -135,67 +155,89 @@
           will not be accepted for donation. Once the reason for the deferral no longer exists and the temporary deferral
           period has lapsed, the donor may return to the blood bank and be re-entered into the system.
           </p>
-          <hr>
-          <h4>If I was deferred once before, am I still ineligible to donate?</h4>
+          </div>
+
+          <button class="accordion">If I was deferred once before, am I still ineligible to donate?</button>
+          <div class="panel">
           <p align="justify">
           If your deferral is of a permanent nature, you will be informed. Otherwise, the deferral time depends upon the
           reason for deferral. Prior to each donation, you will be given a mini-physical and medical interview. At that
           time, it will be determined if you are eligible to donate blood on that particular day.
           </p>
-          <hr>
-          <h4>What are some of the reasons for permanent deferral?</h4>
+          </div>
+
+          <button class="accordion">What are some of the reasons for permanent deferral?</button>
+          <div class="panel">
           <p>- Hepatitis B or C infection.</p>
           <p>- HIV infection.</p>
           <p>- Having sexual contact with a person infected with HIV</p>
           <p>- Having multiple sex partners/ patronizing sex workers</p>
           <p>- Serious chronic illness (heart and lung diseases)</p>
-          <hr>
-          <h4>Can a person who just had his/ her tooth extracted donate blood?</h4>
+          </div>
+          
+          <button class="accordion">Can a person who just had his/ her tooth extracted donate blood?</button>
+          <div class="panel">
           <p>
           He/ She will be temporarily deferred for a year.
           </p>
-          <hr>
-          <h4>If I just received a flu shot, can I donate blood?</h4>
+          </div>
+          
+          <button class="accordion">If I just received a flu shot, can I donate blood?</button>
+          <div class="panel">
           <p>
           Yes. There is no waiting period to donate after receiving a flu shot.
           </p>
-          <hr>
-          <h4>If I have a cold flu, can I donate blood?</h4>
+          </div>
+          
+          <button class="accordion">If I have a cold flu, can I donate blood?</button>
+          <div class="panel">
           <p align="justify">
           In order to donate, blood centers require that you should be generally in good health (symptom-free); thus, it is
           important that you are feeling well.
           </p>
-          <hr>
-          <h4>Can I still donate if I have high blood pressure?</h4>
+          </div>
+          
+          <button class="accordion">Can I still donate if I have high blood pressure?</button>
+          <div class="panel">
           <p>
           Yes, if your blood pressure is under control and within the limits set in the donation guidelines.
           </p>
-          <hr>
-          <h4>What if I am taking aspirin or medication prescribed by my doctor?</h4>
+          </div>
+          
+          <button class="accordion">What if I am taking aspirin or medication prescribed by my doctor?</button>
+          <div class="panel">
           <p align="justify">
           Aspirin and Ibuprofen will not affect a whole blood donation. Apheresis platelet donors, however, must not
           take aspirin or aspirin products 36 hours prior to the donation. Many other medications are acceptable; but
           it is recommended that you call the blood center ahead of time to inquire about the type of medication you
           are taking.
           </p>
-          <hr>
-          <h4>What if I have Anemia?</h4>
+          </div>
+
+          <button class="accordion">What if I have Anemia?</button>
+          <div class="panel">
           <p align="justify">
           You cannot give blood if you have anemia. However, this can often be a temporary condition. Your hemoglobin
           level will be tested before you donate, in order to make sure that it is within an acceptable range.
           </p>
-          <hr>
-          <h4>How long does it take to donate blood?</h4>
+          </div>
+
+          <button class="accordion">How long does it take to donate blood?</button>
+          <div class="panel">
           <p>
           The whole process of donating blood will only take an average of 25 minutes.
           </p>
-          <hr>
-          <h4>Will I put on weight after blood donation?</h4>
+          </div>
+
+          <button class="accordion">Will I put on weight after blood donation?</button>
+          <div class="panel">
           <p>
           No. All you put on is the feeling of satisfaction because you have helped someone.
           </p>
-          <hr>
-          <h4>What other types of tests are done on the blood?</h4>
+          </div>
+
+          <button class="accordion">What other types of tests are done on the blood?</button>
+          <div class="panel">
           <p align="justify">
           Your blood is tested to determine your blood type—classified as A, B, AB, and O—and your Rh factor.
           The Rh factor refers to the presence or absence of a specific antigen, a substance capable of stimulating an
@@ -203,8 +245,10 @@
           antigen or you do not. This information is important to know, because your blood type and Rh factor must be
           compatible with the blood type and Rh factor of the person receiving your blood.
           </p>
-          <hr>
-          <h4>What is the most common blood type?</h4>
+          </div>
+
+          <button class="accordion">What is the most common blood type?</button>
+          <div class="panel">
           <p align="justify">
           The approximate distribution of blood types in the Philippines population is as follows (though distribution 
           may be different for specific racial and ethnic groups):
@@ -214,8 +258,10 @@
           <p>B Rh-positive --- 24-25 percent</p>
           <p>AB Rh-positive --- 4-6 percent</p>
           <p>Rh-negative group --- Less than 1 percent</p>
-          <hr>
-          <h4>What fees are associated with blood?</h4>
+          </div>
+
+          <button class="accordion">What fees are associated with blood?</button>
+          <div class="panel">
           <p align="justify">
           While the donated blood is free, there are significant costs associated with the collection, testing, 
           labeling, preparation of components, and storage of blood. In addition to these, charges are also incurred
@@ -223,8 +269,10 @@
           charged to recover these costs. Blood processing fees collected are in conformance with the stipulated 
           allowable fees as mandated by the Department of Health.
           </p>
-          <hr>
-          <h4>What can you do if you are not eligible to donate?</h4>
+          </div>
+
+          <button class="accordion">What can you do if you are not eligible to donate?</button>
+          <div class="panel">
           <p align="justify">
           While a given individual may be unable to donate, he or she may be able to recruit a suitable donor. 
           PRC Blood Banks are always in need of volunteers to assist during blood donations, or to organize mobile 
@@ -232,12 +280,16 @@
           welcome, to help ensure that blood banks can continue providing safe blood to those in need, most 
           especially to indigent patients.
           </p>
-          <hr>
-          <h4>How can I host a mobile blood donation activity at work, school, church or community?</h4>
+          </div>
+
+          <button class="accordion">How can I host a mobile blood donation activity at work, school, church or community?</button>
+          <div class="panel">
           <p align="justify">
           Kindly refer to the Blood Services Facility near you. Contact the blood center in order to learn more about
           the requirements.
           </p>
+          </div>
+          
         </div>
 
         <div class="tab-pane active" id="4">
@@ -249,7 +301,22 @@
 
 <hr></hr>
 
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+</script>
 
 
 <!-- Bootstrap core JavaScript
